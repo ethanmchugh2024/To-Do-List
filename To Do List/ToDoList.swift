73b477f70bcc8e256f4236bae29c1ef73d87c2a8
Ethan McHugh <ethanmchugh2024@gmail.com>
2022-07-25 09:59:52 -0500

    Created custom ToDoList class

---
+++ To Do List/ToDoList.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoList.swift
+//  To Do List
+//
+//  Created by Ethan McHugh on 7/25/22.
+//
+
+import Foundation
+
+class ToDoList: ObservableObject {
+
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+ }
